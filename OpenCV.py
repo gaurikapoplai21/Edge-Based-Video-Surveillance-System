f43a082d7166c42a9127305f86bfe59d82f211f9
gaurika poplai <gaurikapoplai@gmail.com>
2021-05-11 16:32:09 +0530

    few changes

---
+++ OpenCV.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+import random
+
+class face_detect:
+	def __init__(self):
+		self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+		self.nose_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_mcs_nose.xml')
+		self.imgno = 0;
+
+	def draw(self, ob, frame):
+		for (x,y,w,h) in ob:
+			cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+	def preprocessing(self, frame):
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		return gray;
+
+	def detect_mask(self, only_face, only_face_color):
+		per = 0.6;
+		x_ = len(only_face);
+		y_ = len(only_face[0]);
+		mask = only_face[int(x_*per):x_-int(x_*0.1), int(y_*0.3):y_-int(y_*0.3)]
+		# mask_blur = cv2.medianBlur(mask, 3)
+		_, gray_thresh = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)
+		total = 0; white = 0; black = 0;
+		for i, row in enumerate(gray_thresh):
+			for j, col in enumerate(row):
+				total += 1;
+				if col == 255:
+					white += 1;
+				else:
+					black += 1;
+		print("white: ", white/total)
+		print("black: ", black/total)
+		if (white/total)*100 > 99 or (black/total)*100 > 99:
+			print("Nice mask")
+			cv2.imwrite('E:/Capstone/Implementation/check/mmsk/'+str(self.imgno)+'.png', only_face_color)
+		else:
+			print("No mask")
+			cv2.imwrite('E:/Capstone/Implementation/check/nmsk/'+str(self.imgno)+'.png', only_face_color)
+		self.imgno += 1;
+		return only_face
+
+	def main(self, frame):
+		if frame is None: return [];
+
+		gray = self.preprocessing(frame);
+
+		face = self.face_cascade.detectMultiScale(gray, 
+			scaleFactor = 1.3,
+		    minNeighbors = 5,
+		    minSize = (20, 20),
+		    flags = cv2.CASCADE_SCALE_IMAGE);
+		
+		for (x,y,w,h) in face:
+			only_face = gray[y:y+h, x:x+w];
+			only_face_color = frame[y:y+h, x:x+w];
+			return self.detect_mask(only_face, only_face_color);
+		return frame;
+
+if __name__ == '__main__':
+	f = face_detect()
+	cap = cv2.VideoCapture('mmsk.mp4')
+	while 1:
+		ret, frame = cap.read()
+		frame = f.main(frame);
+		if len(frame): cv2.imshow('Mask Detection', frame)
+		else: break
+		k = cv2.waitKey(1) & 0xff
+		if k == 27: break
+
+cap.release()
+cv2.destroyAllWindows()
--- detect_mask_video.py
+++ detect_mask_video.py
@@ -9,6 +9,7 @@ import time
 import cv2
 import os
 
+i = 0
 def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
 	# from it
@@ -38,26 +39,30 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 		if confidence > 0.5:
 			# compute the (x, y)-coordinates of the bounding box for
 			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+				# ensure the bounding boxes fall within the dimensions of
+				# the frame
+				(startX, startY) = (max(0, startX), max(0, startY))
+				(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
+				# extract the face ROI, convert it from BGR to RGB channel
+				# ordering, resize it to 224x224, and preprocess it
+				face = frame[startY:endY, startX:endX]
+				#picname = "picture" + str(i) + ".png"
+				#cv2.imwrite(picname,face)
+				face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+				face = cv2.resize(face, (224, 224))
+				face = img_to_array(face)
+				face = preprocess_input(face)
+				#cv2.imwrite("picture.jpg",face)
 
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+				# add the face and bounding boxes to their respective
+				# lists
+				faces.append(face)
+				locs.append((startX, startY, endX, endY))
+				#i += 1
 
 	# only make a predictions if at least one face was detected
 	if len(faces) > 0:
@@ -69,6 +74,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 	# return a 2-tuple of the face locations and their corresponding
 	# locations
+	
 	return (locs, preds)
 
 # load our serialized face detector model from disk
@@ -97,23 +103,26 @@ while True:
 	# loop over the detected face locations and their corresponding
 	# locations
 	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
-		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
+			# unpack the bounding box and predictions
+			(startX, startY, endX, endY) = box
+			(mask, withoutMask) = pred
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label = "Mask" if mask > withoutMask else "No Mask"
-		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+			# determine the class label and color we'll use to draw
+			# the bounding box and text
+			label = "Mask" if mask > withoutMask else "No Mask"
+			color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+			if(label == "No Mask"):
+				picname = "picture" + str(i) + ".png"
+				cv2.imwrite(picname,frame)
+				i += 1
+			# include the probability in the label
+			label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-		# include the probability in the label
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-		# display the label and bounding box rectangle on the output
-		# frame
-		cv2.putText(frame, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+			# display the label and bounding box rectangle on the output
+			# frame
+			cv2.putText(frame, label, (startX, startY - 10),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
