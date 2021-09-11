c4ade439d3059e838b5329e37c04021e937a4d56
PrithwishNag <prithwish.nag@gmail.com>
2021-09-11 12:01:40 +0530

    Test Socket

--- t.py
+++ t.py
@@ -1,7 +1,17 @@
+import socket
+
+import cv2
 import json
-import subprocess
 
-l = [1, 2, 1, 5.6]
-l = json.dumps(l)
-
-subprocess.call('python t1.py "%s"' % l, shell=True)
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+t = 2
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while t:
+        img = json.dumps(
+            cv2.cvtColor(cv2.imread("output/nmsk/5.png"), cv2.COLOR_BGR2GRAY).tolist()
+        ).encode()
+        print(t)
+        s.sendall(img + b"|")
+        t -= 1
